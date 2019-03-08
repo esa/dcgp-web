cfefde5e58fc1638c4fcd19bc9b8b3e2ff381263
mikeheddes <mikeheddes@gmail.com>
2019-03-08 17:08:32 +0100

    Updates

---
+++ precache-manifest.46917583a981bfe99b1ecb062203aa2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6cb91ccce31f9259bb30",
+    "url": "/dcgp-web/static/js/runtime~main.6cb91ccc.js"
+  },
+  {
+    "revision": "f2e466f8f7e9e4acd70a",
+    "url": "/dcgp-web/static/js/main.f2e466f8.chunk.js"
+  },
+  {
+    "revision": "ded891f3e57cb9109fdd",
+    "url": "/dcgp-web/static/js/2.ded891f3.chunk.js"
+  },
+  {
+    "revision": "f2e466f8f7e9e4acd70a",
+    "url": "/dcgp-web/static/css/main.ce6f6f01.chunk.css"
+  },
+  {
+    "revision": "959f7bface5bf12a5926c49b19843c55",
+    "url": "/dcgp-web/index.html"
+  }
+];
