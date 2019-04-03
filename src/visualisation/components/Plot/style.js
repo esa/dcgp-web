c83f745a328f37261eba5251248dd9bd9b1e7eca
Mike Heddes <mikeheddes@gmail.com>
2019-04-03 09:16:06 +0200

    Feature copy equation (#29)
    * Remove noise bias
    
    * Add copy prediction equation button

--- src/visualisation/components/Plot/style.js
+++ src/visualisation/components/Plot/style.js
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 import { up } from 'styled-breakpoints'
 import { LineChart } from 'recharts'
 import unStyledGridContainer from '../../../components/GridContainer'
+import { transparentize as fade } from 'polished'
 
 export const StyledLineChart = styled(LineChart)`
   .recharts-curve {
@@ -39,5 +40,28 @@ export const GridContainer = styled(unStyledGridContainer)`
 
   ${up('sm')} {
     grid-column-end: span 2;
+  }
+`
+
+export const CopyButton = styled.button`
+  color: ${({ theme }) => theme.primary};
+  border-radius: 50%;
+  background: none;
+  border: none;
+  padding: 0;
+  font: inherit;
+  cursor: pointer;
+  outline: inherit;
+  transition: color 150ms ease-out;
+  box-sizing: border-box;
+  -webkit-appearance: none;
+
+  &:hover {
+    text-decoration: underline;
+  }
+
+  &:active {
+    transition: color 80ms ease-out;
+    color: ${({ theme }) => fade(0.3, theme.primary)};
   }
 `
