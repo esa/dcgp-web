9607388e0a796b161dcd0063daf3061b9ffda966
mikeheddes <mikeheddes@gmail.com>
2019-04-25 10:04:57 +0200

    Add advanced mode parameter options

--- src/settings/components/Algorithm/Algorithm.js
+++ src/settings/components/Algorithm/Algorithm.js
@@ -1,36 +1,72 @@
 import React, { useCallback } from 'react'
 import { useRedux } from '../../../hooks'
-import { setAlgorithm, algorithmsById } from '../../actions'
+import { setAlgorithm, algorithmsById, changeParameter } from '../../actions'
 import { algorithmSelector } from '../../selectors'
+import { isAdvancedModeSelector } from '../../../ui/selectors'
+import QuantityCounter from '../../../ui/components/QuantityCounter'
 import Container from '../Container'
+import SubHeader from '../SubHeader'
 import Radio from '../../../icons/Radio'
-import List from '../List'
-import { Row } from './style'
+import List, { Row } from '../List'
 import { capitalize } from '../../../utils/string'
 
 const algorithmIds = Object.keys(algorithmsById)
 
 const mapStateToProps = {
   algorithm: algorithmSelector,
+  isAdvancedMode: isAdvancedModeSelector,
 }
 
 const Algorithm = () => {
-  const { dispatch, algorithm } = useRedux(mapStateToProps)
+  const { dispatch, algorithm, isAdvancedMode } = useRedux(mapStateToProps)
 
   const handleChange = useCallback(id => () => dispatch(setAlgorithm(id)), [
     dispatch,
   ])
 
+  const parameters = algorithmsById[algorithm.id].parameters
+
+  const handleSettingChange = useCallback(
+    id => newValue => dispatch(changeParameter(id, newValue)),
+    [dispatch]
+  )
+
   return (
     <Container title="Algorithm">
       <List>
         {algorithmIds.map(algorithmId => (
-          <Row key={algorithmId} onClick={handleChange(algorithmId)}>
-            <Radio checked={algorithm.id === algorithmId} />
+          <Row
+            css="cursor: pointer;"
+            key={algorithmId}
+            onClick={handleChange(algorithmId)}
+          >
+            <Radio
+              css="margin-right: 8px;"
+              checked={algorithm.id === algorithmId}
+            />
             {capitalize(algorithmsById[algorithmId].label)}
           </Row>
         ))}
       </List>
+      {isAdvancedMode && parameters && (
+        <>
+          <SubHeader>Parameters</SubHeader>
+          <List>
+            {Object.keys(parameters).map(parameterId => (
+              <Row key={parameterId}>
+                {capitalize(parameters[parameterId].label)}
+                <div css="flex-grow: 1;" />
+                <QuantityCounter
+                  value={algorithm.byId[algorithm.id][parameterId]}
+                  onChange={handleSettingChange(parameterId)}
+                  min={parameters[parameterId].min}
+                  max={parameters[parameterId].max}
+                />
+              </Row>
+            ))}
+          </List>
+        </>
+      )}
     </Container>
   )
 }
