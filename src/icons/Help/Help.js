47bf54e6172fe556d708b77b8f7a9d7bc2e0c367
mikeheddes <mikeheddes@gmail.com>
2019-05-09 09:47:32 +0200

    Add new Icons Eye, Fold and Help

---
+++ src/icons/Help/Help.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Base from '../Base'
+
+const Help = props => {
+  let path =
+    'M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 17h-2v-2h2v2zm2.07-7.75l-.9.92C13.45 12.9 13 13.5 13 15h-2v-.5c0-1.1.45-2.1 1.17-2.83l1.24-1.26c.37-.36.59-.86.59-1.41 0-1.1-.9-2-2-2s-2 .9-2 2H8c0-2.21 1.79-4 4-4s4 1.79 4 4c0 .88-.36 1.68-.93 2.25z'
+
+  if (props.variant === 'outline') {
+    path =
+      'M11 18h2v-2h-2v2zm1-16C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm0-14c-2.21 0-4 1.79-4 4h2c0-1.1.9-2 2-2s2 .9 2 2c0 2-3 1.75-3 5h2c0-2.25 3-2.5 3-5 0-2.21-1.79-4-4-4z'
+  }
+
+  return (
+    <Base {...props}>
+      <path fill="none" d="M0 0h24v24H0z" />
+      <path d={path} />
+    </Base>
+  )
+}
+
+Help.propTypes = {
+  variant: PropTypes.oneOf(['default', 'outline']),
+}
+
+Help.defaultProps = {
+  variant: 'default',
+}
+
+export default Help
